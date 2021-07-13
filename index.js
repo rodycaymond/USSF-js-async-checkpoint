da059f17fd199f7a7ef8316ec4b2860dc0b02d2f
rodycaymond <58873401+rodycaymond@users.noreply.github.com>
2021-07-13 17:42:03 -0400

    Add files via upload
    final commit with promise all

--- index.js
+++ index.js
@@ -3,19 +3,55 @@ const fetch = require('node-fetch');
 
 const pokeFile = process.argv[2];
 
-let pokeNames =()=>{
-    return new Promise((resolve,reject)=>{
-        fs.readFile(pokeFile, 'utf8',(err,data)=>{
-            if (err){
-                reject(err);
-            } else {
-                let lines = data.split('\n');
-                resolve(lines);
-            }
-        })   
-    }).then(names=>{
+// let pokeNames =()=>{
+//     return new Promise((resolve,reject)=>{
+//         fs.readFile(pokeFile, 'utf8',(err,data)=>{
+//             if (err){
+//                 reject(err);
+//             } else {
+//                 let lines = data.split('\n');
+//                 resolve(lines);
+//             }
+//         })   
+//     }).then(names=>{
+//         let format = '';
+//         names.forEach((name,index)=>{
+//             fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+//             .then(response=>response.json())
+//             .then(data=>{
+//                 let pokeTypes = data.types.map(element=>{
+//                     return element.type.name;
+//                 });
+//                 let nameFormat = name[0].toUpperCase() + name.slice(1);
+//                 format += nameFormat + ': ' + pokeTypes.join(', ') + '\n';
+//                 console.clear();
+//                 console.log(format);
+//             });
+//         });
+//     });
+// };
+// pokeNames();
+
+//Promise.all()???
+
+
+const promise1 = new Promise((resolve,reject)=>{
+    fs.readFile(pokeFile, 'utf8',(err,data)=>{
+        if (err){
+            reject(err);
+        } else {
+            let lines = data.split('\n');
+            resolve(lines);
+        }
+    });
+});
+
+console.log(promise1);
+
+Promise.all([promise1]).then(names=>{
+    console.log(names)
         let format = '';
-        names.forEach((name,index)=>{
+        names[0].forEach((name,index)=>{
             fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
             .then(response=>response.json())
             .then(data=>{
@@ -25,11 +61,9 @@ let pokeNames =()=>{
                 let nameFormat = name[0].toUpperCase() + name.slice(1);
                 format += nameFormat + ': ' + pokeTypes.join(', ') + '\n';
                 console.clear();
-                console.log(format);
+                setTimeout(() => {  console.log(format); }, 2000);
             });
         });
     });
-};
-pokeNames();
 
-//Promise.all()???
+//pokeNames();
